e67b928425be9062d355db6958c6669aec124e0b
Durgesh <kumardurgesh786@gmail.com>
2021-07-21 23:40:58 +0530

    database connectivity done postgres

---
+++ src/main/java/com/example/CabPortal/CabPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.CabPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CabPortal/CabPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CabPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
